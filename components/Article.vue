e72f046b45dba6836ec1112ed13bb864c019795b
adamlwomble <adam@adamwomble.com>
2021-02-04 14:23:24 -0600

    fix: Update Article component to add root element and property types.

--- components/Article.vue
+++ components/Article.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+  <article></article>
+</template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
@@ -6,13 +8,13 @@ import * as Interfaces from '@/interfaces'
 
 @Component
 export default class Article extends Vue implements Interfaces.Article.Base {
-  @Prop() id!: number
-  @Prop() authors!: Interfaces.Author.Base[]
-  @Prop() categories!: Interfaces.Category.Base[]
-  @Prop() description!: string
-  @Prop() feature!: Interfaces.Image.Base
-  @Prop() subtitle!: string
-  @Prop() title!: string
+  @Prop(Number) readonly id!: number
+  @Prop(Array) readonly authors!: Interfaces.Author.Base[]
+  @Prop(Array) categories!: Interfaces.Category.Base[]
+  @Prop(String) description!: string
+  @Prop(Object) feature!: Interfaces.Image.Base
+  @Prop(String) subtitle!: string
+  @Prop(String) title!: string
 
   created() {}
 }
