8c642167ea34573c8915eba90e939f4bdbf783aa
adamlwomble <adam@adamwomble.com>
2021-02-10 22:19:41 -0600

    feat: Update tailwindcss configuration to add typography plugin and begin customizing theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,36 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    container: {
+      center: true
+    },
+    extend: {},
+    fontSize: {
+      xs: '1rem',
+      sm: '1rem',
+      tiny: '1rem',
+      base: '1rem',
+      lg: '1.5rem',
+      xl: '2rem',
+      '2xl': '2.5rem',
+      '3xl': '3rem',
+      '4xl': '3.5rem',
+      '5xl': '4rem',
+      '6xl': '4.5rem',
+      '7xl': '5rem'
+    },
+    spacing: {
+      1: '8px',
+      2: '16px',
+      3: '24px',
+      4: '32px',
+      5: '40px',
+      6: '56px',
+      7: '64px'
+    }
   },
   variants: {
     extend: {}
   },
-  plugins: []
+  plugins: [require('@tailwindcss/typography')]
 }
