d065af0906777d3f00a8fae3dd3eb633b234e6f3
adamlwomble <adam@adamwomble.com>
2021-02-15 18:54:11 -0600

    fix: Update and delete pages to ensure server-side rendering and static site generation work
    - Update index
    - Delete (currently) unused pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="container">
-    <p v-if="alertShow" :class="alertClassList" role="alert">
-      {{ alertMessage }}
-    </p>
+    <alert-base v-if="!alert.success" v-bind="alert" class="mt-4"></alert-base>
 
-    <section class="grid grid-cols-2 gap-8 mt-4">
+    <section v-show="base.length > 1" class="grid grid-cols-2 gap-8 mt-4">
       <insight-base
         v-for="insight in base"
         :key="insight.id"
@@ -16,60 +14,20 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { mapActions, mapState } from 'vuex'
-import { storeModuleKeyGet } from '@/utilities'
+import { mapState } from 'vuex'
+import { storeDispatchKeyGet, storeModuleInstanceKeyGet } from '@/utilities'
 
-const storeModuleKey = storeModuleKeyGet('insights')
+const storeModuleInstanceKey = storeModuleInstanceKeyGet('insights')
 
 export default Vue.extend({
-  data() {
-    const alerted: boolean = false
-
-    return {
-      alerted
-    }
+  async asyncData({ store }) {
+    await store.dispatch(
+      storeDispatchKeyGet(storeModuleInstanceKey, ['base'], 'GET')
+    )
   },
 
   computed: {
-    ...mapState(storeModuleKey, ['acting', 'alert', 'base']),
-
-    alertClassList(): (null | string)[] {
-      return [
-        this.alerted ? null : 'animate-pulse',
-        this.alert.error ? 'bg-red-300' : 'bg-green-300',
-        'px-3',
-        'py-2',
-        'mx-auto',
-        'rounded-lg',
-        'mt-4',
-        'shadow-md',
-        'flex',
-        'items-center',
-        this.alert.error ? 'text-red-900' : 'text-green-900'
-      ]
-    },
-
-    alertMessage(): string {
-      return this.alert.error ? this.alert.error : this.alert.success
-    },
-
-    alertShow(): boolean {
-      return this.alert.error || this.alert.success
-    }
-  },
-
-  created() {
-    this.baseGet().then(() => {
-      if (this.alert.error || this.alert.success) this.alertedTimeout()
-    })
-  },
-
-  methods: {
-    ...mapActions(storeModuleKey, ['baseGet']),
-
-    alertedTimeout(): void {
-      setTimeout(() => (this.alerted = !this.alerted), 2000)
-    }
+    ...mapState(storeModuleInstanceKey, ['acting', 'alert', 'base'])
   }
 })
 </script>
