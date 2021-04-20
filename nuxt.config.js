e561915a9786f56e02d895017b937e458f243e9b
Adam Womble <adam@adamwomble.com>
2021-04-20 18:26:30 -0500

    chore: update dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Our framework of services is your platform for success.'
+      }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
