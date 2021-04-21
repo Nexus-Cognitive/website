f89c045cfc194909b36346a8874c4d8fb75e10fd
Adam Womble <adam@adamwomble.com>
2021-04-21 06:28:42 -0500

    feat: update slide base interface to add order for controlling sorting

--- interfaces/slideBase.ts
+++ interfaces/slideBase.ts
@@ -1,9 +1,10 @@
-import type { ColorRelationT, ImageRelationT } from '@/types'
+import type { ColorRelationT, ImageRelationT, VideoRelationT } from '@/types'
 import type { ResourceBaseI } from '@/interfaces'
 
 export interface SlideBaseI extends ResourceBaseI {
   backgroundColor?: ColorRelationT
   body: string
   image?: ImageRelationT
-  video?: string
+  order: number
+  video?: VideoRelationT
 }
