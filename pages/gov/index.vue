2bf9a05faee94717e1d9a3691a7f8464e2334b35
Adam Womble <adam@adamwomble.com>
2021-04-23 16:55:04 -0500

    feat: create /gov page and empty layout as temporary placeholders

---
+++ pages/gov/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <a id="anchor" href="https://dev-nexus-corp-blog.azurewebsites.net/gov/"></a>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  layout: 'empty',
+
+  mounted(): void {
+    document.getElementById('anchor')?.click()
+  }
+})
+</script>
