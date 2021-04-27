5ac8e3064eb2024704d14c675d7f516c5613e962
Adam Womble <adam@adamwomble.com>
2021-04-26 19:59:16 -0500

    feat: update date format filter to match styling of Figma prototypes

--- plugins/dateFormatFilter.ts
+++ plugins/dateFormatFilter.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 
-const IntlDateTimeFormatter = new Intl.DateTimeFormat([])
+const IntlDateTimeFormatter = new Intl.DateTimeFormat('en-US', {
+  day: 'numeric',
+  month: 'short',
+  year: 'numeric'
+})
 
 Vue.filter('dateFormat', (value: string) => {
   return IntlDateTimeFormatter.format(new Date(value))
