662e7a784c602418fb17413c34b71f52f9dea45a
Chris Gardner <chrisgdesigns@outlook.com>
2021-04-30 18:03:38 -0500

    feat: Create Gov Page
    - Add Gov link to NavigationBase
    - Add optional layout for Gov page for StrategyBase
    - Add optional layout for Gov page for AuthorCard
    - Fix alt text typo in empower.json
    - Added images for Gov page
    - Created JSON file for Gove page HeroBase
    - Created 'link-button' class in Tailwind.css (used said class on Gov page, as well as updated button on See Our Framework section on home page

--- components/AuthorCard.vue
+++ components/AuthorCard.vue
@@ -1,23 +1,51 @@
 <template>
-  <article class="text-black">
-    <figure>
-      <ImageBase class="h-14 w-14" v-bind="image" />
-      <figcaption class="mt-2">
-        <span class="font-bold tracking-wider uppercase">{{ title }}</span
-        ><br /><span class="font-mono" v-html="position"></span>
-      </figcaption>
-    </figure>
-    <div class="flex items-center mt-1 text-gray-dark">
-      <a :href="`mailto:${email}`" class="mr-2"><IconEmail /></a>
-      <a :href="linkedIn"
-        ><ImageBase
-          src="/images/linked-in.svg"
-          alt="LinkedIn Logo"
-          :height="32"
-          :width="32"
-      /></a>
-    </div>
-  </article>
+  <!-- Default AuthorCard -->
+  <div v-if="!gov">
+    <article class="text-black">
+      <figure>
+        <ImageBase class="h-14 w-14" v-bind="image" />
+        <figcaption class="mt-2">
+          <span class="font-bold tracking-wider uppercase">{{ title }}</span
+          ><br /><span class="font-mono" v-html="position"></span>
+        </figcaption>
+      </figure>
+      <div class="flex items-center mt-1 text-gray-dark">
+        <a :href="`mailto:`" class="mr-2"><IconEmail /></a>
+        <a :href="linkedIn"
+          ><ImageBase
+            src="/images/linked-in.svg"
+            alt="LinkedIn Logo"
+            :height="32"
+            :width="32"
+        /></a>
+      </div>
+    </article>
+  </div>
+  <!-- Gov AuthorCard -->
+  <div v-else>
+    <article class="text-white">
+      <figure>
+        <ImageBase class="h-14 w-14" v-bind="image" />
+        <figcaption class="mt-2">
+          <span class="font-bold tracking-wider uppercase"
+            >GOV POC: {{ title }}</span
+          ><br /><span class="font-mono" v-html="position"></span
+          ><span> / Partner</span>
+        </figcaption>
+      </figure>
+      <div class="flex items-center mt-1 text-white">
+        <a :href="`mailto:`" class="mr-2"><IconEmail /></a>
+        <a :href="linkedIn" class="linkedin-link"
+          ><ImageBase
+            class="linkedin-logo"
+            src="/images/linked-in.svg"
+            alt="LinkedIn Logo"
+            :height="32"
+            :width="32"
+        /></a>
+      </div>
+    </article>
+  </div>
 </template>
 
 <script lang="ts">
@@ -29,6 +57,11 @@ export default Vue.extend({
     image: {
       required: true,
       type: Object as PropType<ImageContentT>
+    },
+
+    gov: {
+      required: false,
+      type: Boolean
     },
 
     position: {
@@ -53,3 +86,9 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style scoped>
+.linkedin-link .linkedin-logo {
+  max-width: 32px;
+}
+</style>
