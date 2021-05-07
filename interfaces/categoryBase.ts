e11fef786607c2a229df70a82e2b37271cae0959
Adam Womble <adam@adamwomble.com>
2021-05-07 02:37:34 -0500

    feat: update category and service base interfaces
    - add service relationships and extended data properties

--- interfaces/categoryBase.ts
+++ interfaces/categoryBase.ts
@@ -1,5 +1,7 @@
+import type { ServiceRelationsT } from '@/types'
 import type { ResourceBaseI } from '@/interfaces'
 
 export interface CategoryBaseI extends ResourceBaseI {
   description: string
+  services: ServiceRelationsT
 }
--- interfaces/serviceBase.ts
+++ interfaces/serviceBase.ts
@@ -1,5 +1,9 @@
+import type { ImageRelationT, ServiceRelationsT } from '@/types'
 import type { ResourceBaseI } from '@/interfaces'
 
 export interface ServiceBaseI extends ResourceBaseI {
+  body: string
   description: string
+  image: ImageRelationT
+  services: ServiceRelationsT
 }
