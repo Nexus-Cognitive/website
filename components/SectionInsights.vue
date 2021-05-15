4904e778c3b6933facad4705d8b181c5b657497a
Adam Womble <adam@adamwomble.com>
2021-05-15 06:07:45 -0500

    feat: create and update components
    - update ArrowBase color so it can be set with text-{color} classes,
    - update ArticleList PropType declaration for Insights and Solutions,
    - create SectionInsights for easily displaying lists of sectioned Insights, and
    - remove stray, empty line from StrategyBase.

---
+++ components/SectionInsights.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="px-4 my-6">
+    <Grid v-if="titleShow" class="container" cols-md="2">
+      <div :class="titleClassList">
+        <ArrowBase class="mr-1" />
+        <h2 class="cross title">{{ title }}</h2>
+      </div>
+    </Grid>
+
+    <ArticleList
+      :articles="insights"
+      class="container mt-4"
+      component="InsightBase"
+    />
+  </section>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import { InsightContentsT } from '@/types'
+
+export default Vue.extend({
+  props: {
+    backgroundColor: {
+      required: true,
+      type: String,
+      validator: (v: string): boolean =>
+        ['blue', 'blue-dark', 'green', 'purple', 'red'].includes(v)
+    },
+
+    insights: {
+      required: true,
+      type: Array as PropType<InsightContentsT>
+    },
+
+    title: {
+      required: true,
+      type: String
+    },
+
+    titleShow: {
+      default: true,
+      type: Boolean
+    }
+  },
+
+  computed: {
+    titleClassList(): string {
+      return `bg-${this.backgroundColor} flex items-center px-2 py-1 text-white`
+    }
+  }
+})
+</script>
