2011e4a25ab9d1d9377a43bbab3008230c503e31
Adam Womble <adam@adamwomble.com>
2021-05-25 02:45:34 -0500

    chore: update NavigationBase to always display mobile navigation and hide on route change

--- components/NavigationBase.vue
+++ components/NavigationBase.vue
@@ -1,22 +1,19 @@
 <template>
-  <nav class="md:flex items-center">
-    <div class="flex flex-wrap items-center md:mr-auto">
+  <nav>
+    <div class="flex flex-wrap items-center">
       <Wordmark class="mr-auto" />
 
-      <button class="md:hidden" @click="hiddenToggle">
+      <button @click="hiddenToggle">
         <IconMenu />
       </button>
     </div>
 
-    <ol
-      class="float-right md:flex md:items-center w-full md:w-auto mt-2 md:mt-0 text-right"
-      :class="listClassList"
-    >
+    <ol class="float-right w-full mt-2 text-right" :class="listClassList">
       <li
-        v-for="(route, index) in routes"
+        v-for="route in routes"
         :key="route.name"
-        class="block mt-1 md:mt-0 sm:text-sm font-title"
-        :class="routeClassListGet(index, route.name)"
+        class="block mt-1 sm:text-sm font-title"
+        :class="routeClassListGet(route.name)"
       >
         <nuxt-link :to="{ name: route.name }">
           {{ route.name | capitalize }}
@@ -62,14 +59,21 @@ export default Vue.extend({
     }
   },
 
+  watch: {
+    $route() {
+      if (!this.hidden) {
+        this.hiddenToggle()
+      }
+    }
+  },
+
   methods: {
     hiddenToggle(): void {
       this.hidden = !this.hidden
     },
 
-    routeClassListGet(index: number, name: string): object {
+    routeClassListGet(name: string): object {
       return {
-        'md:mr-4': index !== this.routeLast,
         underline: name === this.$route.name
       }
     }
