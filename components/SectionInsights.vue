e9b77183a36b2eb64278f7788f71a92329712200
Adam Womble <adam@adamwomble.com>
2021-05-25 01:34:27 -0500

    chore: update SectionInsights to set title color, remove backgroundColor, remove titleBlock, and correct spacing

--- components/SectionInsights.vue
+++ components/SectionInsights.vue
@@ -1,15 +1,18 @@
 <template>
-  <section class="px-4 my-6">
+  <section class="px-4 mt-6">
     <Grid v-if="titleShow" class="container" :cols-md="colsMd">
-      <div :class="titleClassList">
-        <ArrowBase :class="arrowClassList" />
-        <h2 class="cross title">{{ title }}</h2>
+      <div class="flex items-center" :class="textColor_">
+        <ArrowBase class="mr-1" />
+
+        <h2 class="cross title">
+          {{ title }}
+        </h2>
       </div>
     </Grid>
 
     <ArticleList
       :articles="insights"
-      class="container mt-4"
+      class="container mt-5"
       component="InsightBase"
       :cols-md="colsMd"
     />
@@ -22,15 +25,6 @@ import { InsightContentsT } from '@/types'
 
 export default Vue.extend({
   props: {
-    backgroundColor: {
-      default: 'blue',
-      type: String,
-      validator: (v: string): boolean =>
-        ['blue', 'blue-dark', 'green', 'purple', 'red'].includes(
-          v.toLowerCase()
-        )
-    },
-
     colsMd: {
       default: 1,
       type: [Number, String],
@@ -43,14 +37,16 @@ export default Vue.extend({
       type: Array as PropType<InsightContentsT>
     },
 
+    textColor: {
+      required: true,
+      type: String,
+      validator: (v: string): boolean =>
+        ['blue', 'blue-dark', 'green', 'purple', 'red'].includes(v)
+    },
+
     title: {
       required: true,
       type: String
-    },
-
-    titleBlock: {
-      default: true,
-      type: Boolean
     },
 
     titleShow: {
@@ -60,18 +56,8 @@ export default Vue.extend({
   },
 
   computed: {
-    arrowClassList(): object {
-      return {
-        'mr-1': true,
-        'text-blue': !this.titleBlock
-      }
-    },
-
-    titleClassList(): string[] {
-      return [
-        'flex items-center',
-        this.titleBlock ? `bg-${this.backgroundColor} px-2 py-1 text-white` : ''
-      ]
+    textColor_(): string {
+      return `text-${this.textColor}`
     }
   }
 })
