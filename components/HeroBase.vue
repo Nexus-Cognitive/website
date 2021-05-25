a25acce04b4647c80d45bb14c2ea7ed658317200
Chris Gardner <chrisgdesigns@outlook.com>
2021-05-25 15:04:00 -0500

    feat: revert videos to use vimeo

--- components/HeroBase.vue
+++ components/HeroBase.vue
@@ -3,7 +3,11 @@
     class="flex justify-center overflow-hidden relative text-center"
     :class="headerClassList"
   >
-    <VideoBase v-if="videoShow" v-bind="video" class="hero-video min-w-full" />
+    <SectionVideo
+      v-if="videoShow"
+      v-bind="video"
+      class="hero-video min-w-full"
+    />
 
     <ImageBase
       v-if="imageShow"
