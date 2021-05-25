8cc161c247522c10e8ea56200ad87807c33c8644
Adam Womble <adam@adamwomble.com>
2021-05-25 17:16:02 -0500

    feat: implement InsightHero on Insight Slug page

--- pages/insights/_slug.vue
+++ pages/insights/_slug.vue
@@ -1,38 +1,23 @@
 <template>
   <article v-if="insight">
-    <HeroBase
-      background-color="blue-dark"
-      :image="insight.cover"
-      section-class-list="xs:h-screen-three-quarter"
-    >
-      <template #default>
-        <div class="text-left">
-          <h1 class="font-bold font-title mt-2 text-sm sm:text-md 2xl:text-lg">
-            {{ insight.title }}
-          </h1>
+    <InsightHero v-bind="insight" :link-show="false" />
 
-          <p class="font-light font-mono mt-1 text-xs sm:text-sm 2xl:text-md">
-            {{ insight.description }}
-          </p>
-          <AuthorList :authors="insight.authors" class="mt-3" />
-        </div>
-      </template>
-    </HeroBase>
-
-    <div class="container px-3 md:px-0 py-6">
-      <NuxtContent
-        class="mx-auto prose prose-blue prose-lg"
-        :document="insight"
-      />
-      <div class="max-w-prose mx-auto">
-        <CategoryList
-          :categories="insight.categories"
-          class="mt-3 text-gray-dark"
+    <div class="px-3 md:px-0 py-4 md:py-6">
+      <div class="sm:container">
+        <NuxtContent
+          class="mx-auto prose prose-blue prose-lg"
+          :document="insight"
         />
+        <div class="max-w-prose mx-auto">
+          <CategoryList
+            :categories="insight.categories"
+            class="mt-3 text-gray-dark"
+          />
 
-        <p class="font-mono mt-3 text-gray-dark">
-          {{ insight.publish | dateFormat }}
-        </p>
+          <p class="font-mono mt-3 text-gray-dark">
+            {{ insight.publish | dateFormat }}
+          </p>
+        </div>
       </div>
     </div>
   </article>
