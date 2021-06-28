9fb55720f59615545a11e15e8c7c40a0130beed1
Chris Gardner <chrisgdesigns@outlook.com>
2021-06-28 10:30:50 -0500

    feat: google analytics implementation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="relative">
+    <client-only>
+      <!-- Google Tag Manager (noscript) -->
+      <noscript
+        ><iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NHNBW44"
+          height="0"
+          width="0"
+          style="display: none; visibility: hidden"
+        ></iframe
+      ></noscript>
+      <!-- End Google Tag Manager (noscript) -->
+    </client-only>
+
     <header class="absolute px-3 sm:px-6 pt-6 sm:pt-9 text-white w-full z-20">
       <NavigationBase />
     </header>
